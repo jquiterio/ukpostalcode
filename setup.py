629d6a0630e38cfd7ceff1fcce28614882176fcb
jquiterio <jquiterio00@gmail.com>
2019-02-16 09:41:23 +0100

    .

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
 
 try:
-    from setuptools.core import setup
-    setup
+    from setuptools import setup
 except ImportError:
     from distutils.core import setup
     
 
-setup (
-       name = 'ukpc',
-       version = '1.0',
-       author = 'Jorge Quiterio',
-       author_mail = 'jquiterio00@gmail.com',
-       py_modules = ['ukpostalcode'],
-       description = 'UK Post Code Format and Validation',
-       url = 'http://guithub.com/jquiterio/ukpc',
-       license = open('LICENSE').read(),
-       long_description=open('README.md').read()
-       )
+setup(
+    name='ukpostalcode',
+    version=1.2,
+    author='Jorge Quiterio',
+    author_mail='eu@jquiterio.eu',
+    py_modules=['ukpostalcode'],
+    description='UK Post Code Format and Validation',
+    url='https://git.jquiterio.eu/jquiterio/ukpostalcode',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
